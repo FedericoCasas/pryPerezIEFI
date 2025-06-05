966c5b14a9040f282594492aae79a3027b23782a
Fde Notebook de Casa <casasfede175@gmail.com>
2025-06-04 22:51:10 -0300

    Agregar archivos de proyecto.

---
+++ Loguin.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace pryPerezIEFI
+{
+    partial class Loguin
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblContraseña = new System.Windows.Forms.Label();
+            this.lblContraseñaPerdida = new System.Windows.Forms.Label();
+            this.lblCrearCuenta = new System.Windows.Forms.Label();
+            this.btnInicio = new System.Windows.Forms.Button();
+            this.txtDniUsuario = new System.Windows.Forms.TextBox();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.stlEstadoConeccion = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(75, 9);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(358, 42);
+            this.lblTitulo.TabIndex = 1;
+            this.lblTitulo.Text = "Introduzca los Datos";
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.Location = new System.Drawing.Point(226, 81);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(74, 42);
+            this.lblUsuario.TabIndex = 2;
+            this.lblUsuario.Text = "Dni";
+            // 
+            // lblContraseña
+            // 
+            this.lblContraseña.AutoSize = true;
+            this.lblContraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContraseña.Location = new System.Drawing.Point(144, 190);
+            this.lblContraseña.Name = "lblContraseña";
+            this.lblContraseña.Size = new System.Drawing.Size(212, 42);
+            this.lblContraseña.TabIndex = 3;
+            this.lblContraseña.Text = "Contraseña";
+            // 
+            // lblContraseñaPerdida
+            // 
+            this.lblContraseñaPerdida.AutoSize = true;
+            this.lblContraseñaPerdida.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContraseñaPerdida.Location = new System.Drawing.Point(279, 410);
+            this.lblContraseñaPerdida.Name = "lblContraseñaPerdida";
+            this.lblContraseñaPerdida.Size = new System.Drawing.Size(223, 25);
+            this.lblContraseñaPerdida.TabIndex = 4;
+            this.lblContraseñaPerdida.Text = "olvidaste tu contraseña?";
+            // 
+            // lblCrearCuenta
+            // 
+            this.lblCrearCuenta.AutoSize = true;
+            this.lblCrearCuenta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCrearCuenta.Location = new System.Drawing.Point(25, 410);
+            this.lblCrearCuenta.Name = "lblCrearCuenta";
+            this.lblCrearCuenta.Size = new System.Drawing.Size(206, 25);
+            this.lblCrearCuenta.TabIndex = 6;
+            this.lblCrearCuenta.Text = "crar una nueva cuenta";
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInicio.Location = new System.Drawing.Point(85, 312);
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.Size = new System.Drawing.Size(326, 53);
+            this.btnInicio.TabIndex = 7;
+            this.btnInicio.Text = "inicio de sesion";
+            this.btnInicio.UseVisualStyleBackColor = true;
+            this.btnInicio.Click += new System.EventHandler(this.btnInicio_Click);
+            // 
+            // txtDniUsuario
+            // 
+            this.txtDniUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDniUsuario.Location = new System.Drawing.Point(164, 126);
+            this.txtDniUsuario.Name = "txtDniUsuario";
+            this.txtDniUsuario.Size = new System.Drawing.Size(181, 49);
+            this.txtDniUsuario.TabIndex = 8;
+            this.txtDniUsuario.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDniUsuario_KeyPress);
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContraseña.Location = new System.Drawing.Point(164, 235);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(181, 49);
+            this.txtContraseña.TabIndex = 9;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.stlEstadoConeccion});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 449);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(524, 22);
+            this.statusStrip1.TabIndex = 10;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // stlEstadoConeccion
+            // 
+            this.stlEstadoConeccion.Name = "stlEstadoConeccion";
+            this.stlEstadoConeccion.Size = new System.Drawing.Size(0, 16);
+            // 
+            // Loguin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 471);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.txtDniUsuario);
+            this.Controls.Add(this.btnInicio);
+            this.Controls.Add(this.lblCrearCuenta);
+            this.Controls.Add(this.lblContraseñaPerdida);
+            this.Controls.Add(this.lblContraseña);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "Loguin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Loguin";
+            this.Load += new System.EventHandler(this.Loguin_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblContraseña;
+        private System.Windows.Forms.Label lblContraseñaPerdida;
+        private System.Windows.Forms.Label lblCrearCuenta;
+        private System.Windows.Forms.Button btnInicio;
+        private System.Windows.Forms.TextBox txtDniUsuario;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel stlEstadoConeccion;
+    }
+}
+
---
+++ Loguin.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+
+namespace pryPerezIEFI
+{
+    public partial class Loguin : Form
+    {
+        OleDbConnection nuevaConeccion = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\\LoguinIEFI.accdb");
+
+        public Loguin()
+        {
+            InitializeComponent();
+
+            //textBox.MaxLength = 10;
+
+            txtContraseña.MaxLength = 10;
+            txtDniUsuario.MaxLength = 10;
+        }
+
+        private void Loguin_Load(object sender, EventArgs e)
+        {
+            try {
+
+                nuevaConeccion.Open();
+                nuevaConeccion.Close();
+                stlEstadoConeccion.Text = "Conectado";
+            }
+            catch (Exception s) {
+            
+                string error = s.ToString();
+                MessageBox.Show(error);
+            }
+
+
+        }
+        private void borrartxt() {
+            txtDniUsuario.Text = "";
+            txtContraseña.Text = "";
+        }
+
+        private void btnInicio_Click(object sender, EventArgs e)
+        {
+            //abrimos la base de datos
+            nuevaConeccion.Open();
+            //guardamos la consulta en un string donde 
+            //seleccionamos  DniEmpleado, Contraseña de la tabla Empleado y revisa si no coincide
+            ////ARMANDO LA CONSULTA
+            string consulta = "select DniEmpleado,Contraseña from Empleado where Contraseña = " + txtContraseña.Text + " and DniEmpleado = " + txtDniUsuario.Text + ";";
+            //creamos un nuevo comando donde guardamos la consulta y la direccion de la base de datos 
+            OleDbCommand Commando = new OleDbCommand(consulta , nuevaConeccion);
+            //creamos OBJETO DE TIPO READER QUE ES COMO UNA TABLA QUE SE LLENARA DESPUES DE LA CONSULTA PARA GUARDAR LOS DATOS SOLICITADOS, PUEDE QUEDAR VACIA DEPENDE LA CONSULTA
+            OleDbDataReader leerDB; //TABLA VACIA UQE SE LLENA POR EL OBJETO COMANDO DESDE LA CONSULTA
+            // lee la consulta para ver que este todo correcto
+            leerDB = Commando.ExecuteReader(); // SE REALIZA LA CONSULTA Y SE CARGA LA TABLA CREADA
+            //HasRows verifia que la tabla este vacia o no, dando un resultado booleano. 
+            Boolean Existe = leerDB.HasRows;
+            if (Existe){
+                MessageBox.Show("Bienvenido al sistema " + txtDniUsuario.Text, "USUARIO AUTORISADO", MessageBoxButtons.OK,MessageBoxIcon.Information);
+
+                MenuPrincipal menuPrincipal = new MenuPrincipal();
+                this.Hide();
+                menuPrincipal.Show();
+
+            }
+            else{
+                MessageBox.Show("ERROR : CONTRASEÑA INCORRECTA");
+                borrartxt();
+            }
+            nuevaConeccion.Close();
+        }
+
+        private void txtDniUsuario_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar)/*verifica que no sea enter o eliminar*/ && !char.IsDigit(e.KeyChar) /*verifica que solo sean numeros*/)
+            {
+                e.Handled = true; // Esta línea cancela la tecla si no es número
+            }
+        }
+    }
+}
